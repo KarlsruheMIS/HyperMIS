0e4c553e0322ec09496d6ddfe0d337c9e5429e32
ernestine-grossmann <ernestine-grossmann@t-online.de>
2026-02-11 10:51:04 +0100

    .

--- run_experiment.sh
+++ run_experiment.sh
@@ -12,21 +12,21 @@ pred_file=$res/RED/pred.csv
 
 t=3600
 n=100
-SEEDS="1 21 203 1002"
+SEEDS=(1 21 203 1002)
 
 ##### REDUCTIONS
 echo "graph,algo,n,m,e,rn,rm,re,offset,time,seed" > "${rred_file}"
-parallel -j ${n} --noswap --delay 1 -k ./build/run_reduce -g {2} -t ${t} -s {1} ::: $SEEDS ::: "${hypergraphs}"/* >> "${rred_file}"
+parallel -j "${n}" --noswap --delay 1 -k ./build/run_reduce -g {2} -t "${t}" -s {1} ::: "${SEEDS[@]}" ::: "${hypergraphs}"/* >> "${rred_file}"
 
 echo "graph,algo,n,m,e,rn,rm,re,offset,time,seed" > "${pred_file}"
-parallel -j ${n} --noswap --delay 1 -k ./build/run_reduce -g {2} -t ${t} -s {1} -p ::: $SEEDS ::: "${hypergraphs}"/* >> "${pred_file}"
+parallel -j "${n}" --noswap --delay 1 -k ./build/run_reduce -g {2} -t "${t}" -s {1} -p ::: "${SEEDS[@]}" ::: "${hypergraphs}"/* >> "${pred_file}"
 
 #####  ILP
 echo "graph,algo,size,time,opt,seed" > "${ilp_file}"
-parallel -j ${n} --noswap --delay 1 -k ./build/run_ilp -g {2} -t ${t} -s {1} ::: $SEEDS ::: "${hypergraphs}"/* >> "${ilp_file}"
+parallel -j "${n}" --noswap --delay 1 -k ./build/run_ilp -g {2} -t "${t}" -s {1} ::: "${SEEDS[@]}" ::: "${hypergraphs}"/* >> "${ilp_file}"
 
 echo "graph,algo,size,time,opt,seed" > "${rilp_file}"
-parallel -j ${n} --noswap --delay 1 -k ./build/run_ilp -r -g {2} -t ${t} -s {1} ::: $SEEDS ::: "${hypergraphs}"/* >> "${rilp_file}"
+parallel -j "${n}" --noswap --delay 1 -k ./build/run_ilp -r -g {2} -t "${t}" -s {1} ::: "${SEEDS[@]}" ::: "${hypergraphs}"/* >> "${rilp_file}"
 
 echo "graph,algo,size,time,opt,seed" > "${pilp_file}"
-parallel -j ${n} --noswap --delay 1 -k ./build/run_ilp -r -p -g {2} -t ${t} -s {1} ::: $SEEDS ::: "${hypergraphs}"/* >> "${pilp_file}"
+parallel -j "${n}" --noswap --delay 1 -k ./build/run_ilp -r -p -g {2} -t "${t}" -s {1} ::: "${SEEDS[@]}" ::: "${hypergraphs}"/* >> "${pilp_file}"
