7353f62a5979c732a26ef675f7a31b91d5b6c7cd
ernestine-grossmann <ernestine-grossmann@t-online.de>
2026-04-07 12:57:32 +0200

    fixed unconfined

--- src/reductions.cpp
+++ src/reductions.cpp
@@ -11,77 +11,140 @@ typedef MISH_algorithm::IS_status IS_status;
 // Test if A and B are equal
 static inline int set_is_equal(const NodeID *A, NodeID a, const NodeID *B, NodeID b)
 {
-    if (b != a)
-        return 0;
+  if (b != a)
+    return 0;
 
-    for (NodeID i = 0; i < a; i++)
-    {
-        if (A[i] != B[i])
-            return 0;
-    }
+  for (NodeID i = 0; i < a; i++)
+  {
+    if (A[i] != B[i])
+      return 0;
+  }
 
-    return 1;
+  return 1;
 }
 
 // Test if A is a subset of B
 static inline int set_is_subset(const NodeID *A, NodeID a, const NodeID *B, NodeID b)
 {
-    if (b < a)
-        return 0;
+  if (b < a)
+    return 0;
 
-    NodeID i = 0, j = 0;
-    while (i < a && j < b && (a - i <= b - j))
+  NodeID i = 0, j = 0;
+  while (i < a && j < b && (a - i <= b - j))
+  {
+    if (A[i] == B[j])
     {
-        if (A[i] == B[j])
-        {
-            i++;
-            j++;
-        }
-        else if (A[i] > B[j])
-        {
-            j++;
-        }
-        else
-        {
-            return 0;
-        }
+      i++;
+      j++;
     }
+    else if (A[i] > B[j])
+    {
+      j++;
+    }
+    else
+    {
+      return 0;
+    }
+  }
 
-    return i == a;
+  return i == a;
 }
 
 // Test if A is a subset of B, ignoring x from A
 static inline int set_is_subset_except_one(const NodeID *A, NodeID a, const NodeID *B, NodeID b, NodeID x)
 {
-    if (b < a - 1)
-        return 0;
+  if (b < a - 1)
+    return 0;
 
-    NodeID i = 0, j = 0;
-    while (i < a && j < b)
+  NodeID i = 0, j = 0;
+  while (i < a && j < b)
+  {
+    if (A[i] == B[j])
     {
-        if (A[i] == B[j])
-        {
-            i++;
-            j++;
-        }
-        else if (A[i] > B[j])
-        {
-            j++;
-        }
-        else if (A[i] == x)
-        {
-            i++;
-        }
-        else
-        {
-            return 0;
-        }
+      i++;
+      j++;
     }
+    else if (A[i] > B[j])
+    {
+      j++;
+    }
+    else if (A[i] == x)
+    {
+      i++;
+    }
+    else
+    {
+      return 0;
+    }
+  }
 
-    if (i < a && A[i] == x)
-        i++;
+  if (i < a && A[i] == x)
+    i++;
 
-    return i == a;
+  return i == a;
+}
+
+// Test if A \ B = \emtpyset (return -1) if A\B = {v} (return v) else return -2
+static inline int set_difference_check(const NodeID *A, NodeID a, const NodeID *B, NodeID b)
+{
+  if (a > b + 1)
+    return -2;
+
+  int difference = 0;
+
+  NodeID differenceNode;
+  NodeID i = 0, j = 0;
+  while (i < a && j < b && difference < 2)
+  {
+    if (A[i] == B[j])
+    {
+      i++;
+      j++;
+    }
+    else if (A[i] > B[j])
+    {
+      j++;
+    }
+    else if (A[i] < B[j])
+    {
+      differenceNode = A[i];
+      difference++;
+      i++;
+    }
+  }
+
+  if (difference == 0 && i == a)
+    return -1;
+
+  if (difference == 1 && i == a)
+    return differenceNode;
+
+  return -2;
+}
+
+// Test if |A \cap B| == 1
+static inline int set_intersection_equal_one(const NodeID *A, NodeID a, const NodeID *B, NodeID b)
+{
+  int intersection = 0;
+  NodeID i = 0, j = 0;
+  while(intersection < 2 && i < a && j < b)
+  {
+    if (A[i] == B[j])
+    {
+      intersection++;
+      i++;
+      j++;
+    }
+    else if (A[i] > B[j])
+      j++;
+    else
+     i++;
+  }
+
+  if (intersection == 1)
+    return 1;
+  else
+    return 0;
 }
 
 bool degree_one_reduction::reduce(MISH_algorithm *mish_alg)
@@ -93,7 +156,8 @@ bool degree_one_reduction::reduce(MISH_algorithm *mish_alg)
   {
     NodeID v = marker.current_element(v_idx);
 
-    if (status.node_status[v] == IS_status::not_set && status.hgraph->Vd[v] < 2)
+    if (status.node_status[v] == IS_status::not_set)
+      if (status.hgraph->Vd[v] < 2 || status.hgraph->Nd[v] < 2)
         mish_alg->set(v, IS_status::included);
   }
 
@@ -109,13 +173,13 @@ bool unconfined_reduction::reduce(MISH_algorithm *mish_alg)
   std::vector<NodeID> S;
   std::vector<NodeID> neighborhood_S;
   neighborhood_S.reserve(status.n);
-  fast_set extend_neighborhood_S(status.n);
+  fast_set set_neighborhood_S(status.n);
   int num_iterations = 0;
 
   for (size_t v_idx = 0; v_idx < marker.current_size(); v_idx++)
   {
-  auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - mish_alg->start_time).count();
-  if (elapsed > TIME_KERNEL_SECONDS)
+    auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - mish_alg->start_time).count();
+    if (elapsed > TIME_KERNEL_SECONDS)
       break;
     NodeID v = marker.current_element(v_idx);
     if (status.node_status[v] != IS_status::not_set)
@@ -124,92 +188,64 @@ bool unconfined_reduction::reduce(MISH_algorithm *mish_alg)
     if (num_iterations >= ITERATIONS_UNCONFINED)
       break;
 
-    if (g->Nd[v] > 20)
-      continue;
+    // if (g->Nd[v] > NEIGHBORS_SIZE)
+    //   continue;
 
     bool v_confined = false;
 
     S.clear();
     neighborhood_S.clear();
-    extend_neighborhood_S.clear();
+    set_neighborhood_S.clear();
 
     NodeID next_node = v;
-    while (!v_confined && status.node_status[v] == IS_status::not_set && S.size() <= CONSTANT_UNCONFINED)
+    while (!v_confined && S.size() <= CONSTANT_UNCONFINED)
     {
       auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - mish_alg->start_time).count();
       if (elapsed > TIME_KERNEL_SECONDS)
         break;
 
       S.push_back(next_node);
-      extend_neighborhood_S.add(next_node);
+      assert(!set_neighborhood_S.get(next_node));
+      set_neighborhood_S.add(next_node);
+      neighborhood_S.push_back(next_node);
 
-      for (NodeID i = 0; i < g->Nd[v]; i++)
+      for (NodeID i = 0; i < g->Nd[next_node]; i++)
       {
-        NodeID neighbor = g->N[v][i];
-        if (g->Nd[neighbor] > NEIGHBORS_SIZE)
-          continue;
+        NodeID neighbor = g->N[next_node][i];
+        if (set_neighborhood_S.add(neighbor))
+          neighborhood_S.push_back(neighbor);
+      }
+      std::sort(neighborhood_S.begin(), neighborhood_S.end());
+      std::sort(S.begin(), S.end());
 
-        neighborhood_S.push_back(neighbor);
-        extend_neighborhood_S.add(neighbor);
+      bool v_unconfined = false;
+      bool u_is_child_of_S = false;
+      NodeID u;
+
+      for (NodeID i = 0; i < neighborhood_S.size() && !u_is_child_of_S; i++)
+      {
+        u = neighborhood_S[i];
+        u_is_child_of_S = set_intersection_equal_one(status.hgraph->N[u], status.hgraph->Nd[u], S.data(), S.size());
       }
 
-      bool found_unconfined = false;
-      v_confined = true;
-      for (NodeID n = 0; n < neighborhood_S.size(); n++)
+      if (!u_is_child_of_S)
+        v_confined = true;
+      else
       {
-        NodeID u = neighborhood_S[n];
-        if (found_unconfined || g->Nd[u] > 20)
-          continue;
-        auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - mish_alg->start_time).count();
-        if (elapsed > TIME_KERNEL_SECONDS)
-          break;
-
-        // calculate number of intersected nodes
-        short intersection_size = 0;
-        int i = 0;
-        while (intersection_size <= 2 && i < S.size())
-        {
-          NodeID s = S[i];
-          if (hypergraph_is_neighbor(g, u, s))
-            intersection_size++;
-
-          i++;
-        }
-
-        if (intersection_size == 1)
-        {
-          // boolean if there exists a node with intersection == 1
-          // calculate difference: N(u) \ N[S]
-          NodeID difference_node;
-          short difference_size = 0;
-          for (NodeID i = 0; i < g->Nd[u]; i++)
-          {
-            NodeID v = g->N[u][i];
-            if (!extend_neighborhood_S.get(v))
-            {
-              difference_size++;
-              if (difference_size == 2)
-                break;
-
-              difference_node = v;
-            }
-          }
-
-          if (difference_size == 1)
-          {
-            next_node = difference_node;
-            v_confined = false;
-          }
-          else if (difference_size == 0)
-          {
-            v_confined = false;
-            found_unconfined = true;
-          }
-        }
+        int unconfined_condition_check = set_difference_check(status.hgraph->N[u], status.hgraph->Nd[u], neighborhood_S.data(), neighborhood_S.size());
+        if (unconfined_condition_check == -1)
+          v_unconfined = true;
+        else if (unconfined_condition_check < -1)
+          v_confined = true;
+        else
+          next_node = unconfined_condition_check;
       }
-      // in this case, v is unconfined
-      if (found_unconfined)
+
+      if (v_unconfined)
+      {
         mish_alg->set(v, IS_status::excluded);
+        break;
+      }
     }
     num_iterations++;
   }
@@ -277,7 +313,7 @@ bool node_domination_reduction::reduce(MISH_algorithm *mish_alg)
         if (g->Vd[u] == 0)
           continue;
 
-        bool is_dominating = set_is_subset_except_one(g->N[v], g->Nd[v], g->N[u], g->Nd[u], u); 
+        bool is_dominating = set_is_subset_except_one(g->N[v], g->Nd[v], g->N[u], g->Nd[u], u);
 
         if (is_dominating)
         {
@@ -314,7 +350,7 @@ bool twin_reduction::reduce(MISH_algorithm *mish_alg)
     for (NodeID i = 1; i < g->Nd[v]; i++)
     {
       NodeID u = g->N[v][i];
-      assert(status.node_status[u]==IS_status::not_set);
+      assert(status.node_status[u] == IS_status::not_set);
       if (g->Vd[minD_neighbor] > g->Vd[u])
         minD_neighbor = u;
     }
@@ -342,7 +378,7 @@ bool twin_reduction::reduce(MISH_algorithm *mish_alg)
     for (NodeID i = 1; i < twins.size(); i++)
       if (min_degree > g->Vd[twins[i]])
         min_degree = g->Vd[twins[i]];
-    
+
     if (twins.size() >= min_degree)
       for (NodeID twin : twins)
         mish_alg->set(twin, IS_status::included);
@@ -371,7 +407,7 @@ bool clique_reduction::reduce(MISH_algorithm *mish_alg)
     for (NodeID i = 0; i < g->Nd[v]; i++)
     {
       NodeID u = g->N[v][i];
-      simplicial = set_is_subset_except_one(g->N[v], g->Nd[v], g->N[u],g->Nd[u], u);
+      simplicial = set_is_subset_except_one(g->N[v], g->Nd[v], g->N[u], g->Nd[u], u);
       if (!simplicial)
         break;
     }
@@ -387,7 +423,7 @@ bool clique_reduction::reduce(MISH_algorithm *mish_alg)
 
 void clear_reduction_vector(std::vector<reduction_ptr> &vec)
 {
-	for (auto &rp : vec)
-		rp.release();
-	vec.clear();
+  for (auto &rp : vec)
+    rp.release();
+  vec.clear();
 }
